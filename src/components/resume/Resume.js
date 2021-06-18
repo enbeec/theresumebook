c4b95dec3a6292e8ce113a2216c574b88c7d351c
Val Currie <contact@valcuri.email>
2021-06-18 11:00:03 -0500

    starting to swap in rebass and styled-components

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,27 +1,28 @@
-import userEvent from "@testing-library/user-event";
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Box, Flex } from "rebass";
+import "./Resume.css";
 
 export const Resume = () => (
   <>
-    <div className="resumeContainer ">
-      <div className="projectsContainer ">
-        <div className="project centered">project 1</div>
-        <div className="project centered">project 2</div>
-        <div className="project centered">project 3</div>
-      </div>
-      <div className="skillsAndExpsContainer ">
-        <div className="skillsContainer ">
+    <Flex className="resumeContainer ">
+      <Flex className="projectsContainer ">
+        <Box>project 1</Box>
+        <Box>project 2</Box>
+        <Box>project 3</Box>
+      </Flex>
+      <Flex>
+        <Flex>
           <div className="skill centered">skill 1</div>
           <div className="skill centered">skill 2</div>
           <div className="skill centered">skill 3</div>
-        </div>
-        <div className="expsContainer ">
+        </Flex>
+        <Flex>
           <div className="exp centered">exp 1</div>
           <div className="exp centered">exp 2</div>
           <div className="exp centered">exp 3</div>
-        </div>
-      </div>
-    </div>
+        </Flex>
+      </Flex>
+    </Flex>
   </>
 );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ResumeBook } from "./components/ResumeBook";
+import { ThemeProvider } from "styled-components";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <ResumeBook />
+      <ThemeProvider theme={trbTheme}>
+        <ResumeBook />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
