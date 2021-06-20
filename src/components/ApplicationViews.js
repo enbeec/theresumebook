c0148053c41d52ca9de2d025b659940f7c9f929b
Val Currie <contact@valcuri.email>
2021-06-20 11:04:55 -0500

    working

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import { Resume } from "./resume/Resume";
 import { Heading } from "rebass";
 
 export const ApplicationViews = () => {
+  const currentUserPath = `/resume/${localStorage.getItem("trb_user")}`;
   return (
     <>
+      {/* HOMEPAGE */}
       <Route exact path="/">
         <Heading sx={{ textAlign: "center" }}>Homepage</Heading>
       </Route>
+      {/* MY RESUME */}
+      <Route exact path={currentUserPath}>
+        <MINE />
+      </Route>
+      {/* VIEW RESUME */}
       <Route exact path="/resume/:userId(\d+)">
         <Resume />
       </Route>
     </>
   );
 };
+
+const MINE = (props) => (
+  <div style={{ textAlign: "center" }}>THIS IS MY RESUME</div>
+);
