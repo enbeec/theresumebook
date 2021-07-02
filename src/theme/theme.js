0fe844b25bc1ced52004bbf8c0193c9f2826e2ea
Val Currie <contact@valcuri.email>
2021-07-02 16:51:41 -0400

    squashin lil bugs from my capstone approval

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -11,7 +11,6 @@ export const theme = {
   },
   radii: ["5px", "10px", "18px"],
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
-  // QUESTION not sure if this is to spec
   fonts: {
     regular: "Iosevka",
     medium: "Iosevka Medium",
